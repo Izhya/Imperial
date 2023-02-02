f935d1986156aaa591e248703a95effd82adb9f2
Tim <Tim@DESKTOP-82U0OMM>
2023-02-02 20:34:29 +0100

    Add project files.

---
+++ Information.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Information
+{
+    
+    public static class Messages
+    {    
+        public static string onStartup = "Welcome to this Imperial units converter.";
+        
+        //Remember to update once more converters are implemented.
+        public static string whatConverterToUse = "Unfortunately, length is the only converter so far, so we will use the length-converter.";
+        public static string formattingExample = "Example input: \n 1 ft in y. \n 1 feet in yards.";
+        public static string lengthConversion = "Please enter what to convert:";
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Information;
+
+namespace ImperialUnitsConverter
+{
+    class Program
+    {
+
+        // TODO: Implement more converters and remove 0 to allow choice.
+        static string userCaseInput = "1";
+        static int userChoiceSwitch;
+        static string userCalculation;
+
+        static bool keepRunning = true;
+        static bool validInput;
+
+        static void Main(string[] args) 
+        {
+            Console.WriteLine(Messages.onStartup);
+            
+            while (keepRunning)
+            {    
+                Console.WriteLine(Messages.whatConverterToUse);
+                validInput = int.TryParse(userCaseInput, out userChoiceSwitch);
+
+                if (validInput)
+                {
+                    switch (userChoiceSwitch)
+                    {
+                        case 1:
+                            Console.WriteLine(Messages.formattingExample);
+                            Console.WriteLine(Messages.lengthConversion);
+                            userCalculation = Console.ReadLine();
+                            return;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, try again.");
+                }
+                
+
+            }
+           
+        }
+    }
+}
